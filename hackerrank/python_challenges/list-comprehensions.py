937576c8b1df62103efbd4ed6b9d12105a574272
MatsLanGoH <mats.lan.pod@googlemail.com>
2016-09-29 23:06:31 +0900

    Added Hackerrank solution(s)

---
+++ hackerrank/python_challenges/list-comprehensions.py
@@ -0,0 +1,15 @@
+"""
+Problem description:
+https://www.hackerrank.com/challenges/list-comprehensions
+
+Solution by Matthias Lambrecht
+"""
+
+x, y, z, n = (int(input()) for _ in range(4))
+
+coordinates = [[a, b, c] for a in range(x + 1)
+               for b in range(y + 1)
+               for c in range(z + 1)
+               if a + b + c != n]
+
+print(coordinates)
--- hackerrank/python_challenges/python-tuples.py
+++ hackerrank/python_challenges/python-tuples.py
@@ -12,8 +12,4 @@ buffer = []
 for el in raw_input().split():
     buffer.append(int(el))
 
-print(buffer)
-print(tuple(buffer))
-
 print(hash(tuple(buffer)))
-
